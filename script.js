851ead3dc14a506569b17aa1ab51ada00804f8ae
nm-woodward <nowoodward15@gmail.com>
2022-03-27 18:46:37 -0700

    Connected number buttons to display text and JS variable

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+
+let displayNumber = 0;
+let storedNumber = 0;
+
+
+
+// Function to store number click as text in displayNumber
+const viwerContent = document.querySelector('.viewer');
+
+function updateDisplayNumber(newAddition) {
+    displayNumber = Number(String(displayNumber) + String(newAddition));
+    viwerContent.textContent = displayNumber;
+}
+
+
+
+// Add updateDisplayNumber to all number buttons
+const numberButtons = document.querySelectorAll('.numberButton');
+
+
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener('mousedown', (e) => {
+        updateDisplayNumber(e.toElement.textContent);
+    })
+});
+
+// Store away displayNumber
+
+function storeDisplay() {
+    storedNumber = displayNumber;
+    displayNumber = 0;
+}
+
+// OPERATOR FUNCTIONS
 function add(a,b) {
     return a + b;
 }
